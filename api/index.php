0fe3da4acea76106f8a3bef68fb9a9e79f07e975
efaiyzett <faiz.alhafidz.fh@gmail.com>
2026-06-24 13:49:58 +0700

    update index php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,32 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Set storage path to /tmp because Vercel is a read-only file system
+$storagePath = '/tmp/storage';
+$directories = [
+    "$storagePath/app/public",
+    "$storagePath/framework/cache/data",
+    "$storagePath/framework/sessions",
+    "$storagePath/framework/testing",
+    "$storagePath/framework/views",
+    "$storagePath/logs",
+];
+
+foreach ($directories as $directory) {
+    if (!is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
+}
+
+$app->useStoragePath($storagePath);
+
+$app->handleRequest(Request::capture());
